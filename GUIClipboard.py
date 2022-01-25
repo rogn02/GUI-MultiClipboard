d9ed4d5599ee2c685c3224b986a3d4bcfbca4861
rogn02 <57052476+rogn02@users.noreply.github.com>
2022-01-25 14:40:40 +0530

    Add files via upload

---
+++ GUIClipboard.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from pathlib import Path
+import clipboard
+from PIL import ImageTk ,Image
+
+a=Path("Images")
+
+def reset1():
+    Entry1.delete(0,'end')
+    clipboard.copy("")
+def reset2():
+    Entry2.delete(0,'end')
+    clipboard.copy("")
+def reset3():
+    Entry3.delete(0,'end')
+    clipboard.copy("")
+
+top=tk.Tk()
+top.geometry("600x450+650+150")
+top.minsize(148, 1)
+top.maxsize(1924, 1055)
+top.resizable(1, 1)
+top.title("GUI Clipboard")
+top.configure(background="#353839")
+
+img1 = ImageTk.PhotoImage(Image.open(a/"copy.png").resize((53,56)))
+img2 = ImageTk.PhotoImage(Image.open(a/"trashcan.png").resize((53,56)))
+img3 = ImageTk.PhotoImage(Image.open(a/"label_image.png")) 
+
+Label1 = tk.Label(top)
+Label1.place(relx=0.033, rely=0.733, height=106, width=562)
+Label1.configure(background="#d9d9d9")
+Label1.configure(image=img3)
+Label1.configure(background="#FFFFFF")
+
+Entry1 = tk.Entry(top)
+Entry1.place(relx=0.033, rely=0.178,height=54, relwidth=0.707)
+Entry1.configure(background="white")
+Entry1.configure(font="TkFixedFont")
+
+
+Button1 = tk.Button(top)
+Button1.place(relx=0.767, rely=0.178, height=53, width=56)
+Button1.configure(background="#d9d9d9")
+Button1.configure(command=lambda :clipboard.copy(Entry1.get()))
+Button1.configure(image=img1)
+
+Button2 = tk.Button(top)
+Button2.place(relx=0.883, rely=0.178, height=53, width=56)
+Button2.configure(background="#d9d9d9")
+Button2.configure(command=reset1)
+Button2.configure(image=img2)
+
+Entry2 = tk.Entry(top)
+Entry2.place(relx=0.033, rely=0.356,height=54, relwidth=0.707)
+Entry2.configure(background="white")
+
+Button3 = tk.Button(top)
+Button3.place(relx=0.767, rely=0.356, height=53, width=56)
+Button3.configure(background="#d9d9d9")
+Button3.configure(command=lambda :clipboard.copy(Entry2.get()))
+Button3.configure(image=img1)
+
+Button4 = tk.Button(top)
+Button4.place(relx=0.883, rely=0.356, height=53, width=56)
+Button4.configure(background="#d9d9d9")
+Button4.configure(command=reset2)
+Button4.configure(image=img2)
+
+Entry3 = tk.Entry(top)
+Entry3.place(relx=0.033, rely=0.533,height=54, relwidth=0.707)
+Entry3.configure(background="white")
+
+Button5 = tk.Button(top)
+Button5.place(relx=0.767, rely=0.533, height=53, width=56)
+Button5.configure(background="#d9d9d9")
+Button5.configure(command=lambda :clipboard.copy(Entry3.get()))
+Button5.configure(image=img1)
+
+Button6 = tk.Button(top)
+Button6.place(relx=0.883, rely=0.533, height=53, width=56)
+Button6.configure(background="#d9d9d9")
+Button6.configure(command=reset3)
+Button6.configure(image=img2)
+
+top.mainloop()
+
+
+
+
